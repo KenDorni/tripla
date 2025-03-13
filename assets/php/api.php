6c367cfb0c44f1feab83900d62e2e924c731d0cc
Luxbaut <frith033@school.lu>
2025-03-13 09:00:57 +0100

    PHP (crud)
    Hier habe ich den CRUD hochgeladen.

---
+++ assets/php/api.php
@@ -0,0 +1,24 @@
+<?php
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+switch ($method) {
+    case "POST":
+        include "crud/create.php";
+        break;
+    case "GET":
+        include "crud/read.php";
+        break;
+    case "PUT":
+        include "crud/update.php";
+        break;
+    case "DELETE":
+        include "crud/delete.php";
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(array("message" => "Method not allowed"));
+        break;
+}
+
+?>
