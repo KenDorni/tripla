ec8e562f52af4ffbf914948de1b5a03950c7edc7
Balou132 <153287358+Balou132@users.noreply.github.com>
2025-03-14 11:24:08 +0100

    First Step for Home CSS
    added 2 folder 4 css and includes
    css header perfect ;D

---
+++ include/header.php
@@ -0,0 +1,17 @@
+<!-- Header -->
+<header>
+    <div class="logo">Tripla</div>
+    <nav>
+        <ul class="nav-links">
+            <li><a href="#" id="">Button1</a></li>
+            <li><a href="#" id="">Button2</a></li>
+            <li><a href="#" id="">Button3</a></li>
+            <li><a> Profile Pic/Login</a></li>
+        </ul>
+    </nav>
+</header>
+
+<!-- Banner -->
+<section class="banner">
+    <h1>Explore, Enjoy, Ease up</h1>
+</section>
