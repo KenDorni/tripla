4d767dfaa3dbd72b21b4eac2ff902e1104d69faf
Balou132 <153287358+Balou132@users.noreply.github.com>
2025-03-27 08:56:31 +0100

    Restructer Page

--- index.php
+++ index.php
@@ -16,43 +16,24 @@ $page = $_GET["page"];
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- HEADER -->
+    <link rel="stylesheet" href="assets/css/header.css">
+
+    <!-- Suchleiste -->
+    <link rel="stylesheet" href="assets/css/searchField.css">
 </head>
 <body>
 <header>
     <?php
-
+    include "include/header.php";
     ?>
 </header>
 <main>
     <?php
-    include "pages/" . $page . ".php";
+    include "include/pages/" . $page . ".php";
     ?>
 </main>
 <footer>
 
 </footer>
-</body>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-
-        <!-- HEADER -->
-        <link rel="stylesheet" href="assets/css/header.css">
-
-        <!-- Suchleiste -->
-        <link rel="stylesheet" href="assets/css/searchField.css">
-
-    </head>
-    <body>
-
-    <?php
-        include "include/header.php";
-        include "include/searchField.php";
-    ?>
-
-
-    </body>
-</html>
