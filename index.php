1e76da3dc7f9720b4f8838f6af4ce6cb7b1aa3ac
DorKe108 <153517509+KenDorni@users.noreply.github.com>
2025-04-04 10:42:17 +0200

    session handling

--- index.php
+++ index.php
@@ -3,9 +3,36 @@ include "assets/php/db_functions.php";
 include "assets/php/db_credentials.php";
 include "assets/php/functions.php";
 
+session_start();
+
+if (isset($_POST["logout"])) {
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+}
+
 $page = $_GET["page"];
 
 
+
+
+
+
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time
+    session_destroy();   // destroy session data in storage
+}
+
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+if (!isset($_SESSION['CREATED'])) {
+    $_SESSION['CREATED'] = time();
+} else if (time() - $_SESSION['CREATED'] > 1800) {
+    // session started more than 30 minutes ago
+    session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
+    $_SESSION['CREATED'] = time();  // update creation time
+}
 
 ?>
 <!doctype html>
